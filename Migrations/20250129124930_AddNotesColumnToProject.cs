77a1c30de8e01be5521c8eee8c26fcb2d8788c27
lirije <lirije11@hotmail.com>
2025-01-29 13:51:00 +0100

    column added to projects table

---
+++ Migrations/20250129124930_AddNotesColumnToProject.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace portfolioApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNotesColumnToProject : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Todo",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Todo",
+                table: "Projects");
+        }
+    }
+}
